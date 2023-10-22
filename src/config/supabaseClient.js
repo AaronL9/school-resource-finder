9ed8d0dce15b73c9c9d95702a9f2bb4b23e60622
Aaron Lomibao <aaron.lomibao09@gmail.com>
2023-10-22 22:30:15 +0800

    update

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://ueohlrqhtxiccauzwaoh.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlb2hscnFodHhpY2NhdXp3YW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY3Njk4NzksImV4cCI6MjAxMjM0NTg3OX0.36WvWAfRBxT6ranj1apTDM8xU_qfXzl62GSVaqtc5wo";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
