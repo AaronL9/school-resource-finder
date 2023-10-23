24978a5d70693a6d4a5c4ecc317118f6b78394bc
Aaron Lomibao <aaron.lomibao09@gmail.com>
2023-10-23 10:13:54 +0800

    comit

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+console.log(import.meta.env.MODE);
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
