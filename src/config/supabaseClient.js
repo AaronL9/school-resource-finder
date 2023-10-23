db7aeee19538ac8f52a44318147331f7fed0fd75
Aaron Lomibao <aaron.lomibao09@gmail.com>
2023-10-23 09:37:50 +0800

    env added

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ueohlrqhtxiccauzwaoh.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlb2hscnFodHhpY2NhdXp3YW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY3Njk4NzksImV4cCI6MjAxMjM0NTg3OX0.36WvWAfRBxT6ranj1apTDM8xU_qfXzl62GSVaqtc5wo";
+const supabaseUrl = import.meta.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
